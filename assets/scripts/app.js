5c590bdd96b374483ee812769fa894e3712b2c6a
acupoftee <teedia31@gmail.com>
2019-06-27 16:10:22 -0400

    Add sign up event in assets/scripts/app.js
    Adding sign up event in assets/scripts/app.js.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,13 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const boardEvents = require('./game/boardEvents')
+const authEvents = require('./auth/authEvents')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
   boardEvents.runGame()
 })
