f3b7178f0f1cd48a3b3a906a1fea2750665e6623
acupoftee <teedia31@gmail.com>
2019-06-27 16:05:36 -0400

    Update assets/scripts/config.js
    Added production and development urls in assets/scripts/config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
+  production: 'https://tic-tac-toe-wdi-production.herokuapp.com',
+  development: 'https://tic-tac-toe-wdi.herokuapp.com/'
 }
 
 if (window.location.hostname === 'localhost') {
