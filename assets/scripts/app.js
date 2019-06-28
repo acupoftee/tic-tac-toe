b5699e3849051a5804c5e6a29a7c2bd26667c7fb
acupoftee <teedia31@gmail.com>
2019-06-28 13:59:51 -0400

    Add Hidden Class to New Game
    Added 'hidden' class to new game button in index.html.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,15 +4,21 @@
 // const example = require('./example')
 const boardEvents = require('./game/boardEvents')
 const authEvents = require('./auth/authEvents')
+const gameEvents = require('./game/gameEvents')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+
+  // AUTHENTICATION EVENTS
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut)
+
+  // GAME EVENTS
+  $('#create-game').on('submit', gameEvents.onCreateGame)
   boardEvents.runGame()
 })
