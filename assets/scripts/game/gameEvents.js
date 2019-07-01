33307895de03c2f18e210969c89fbb14108e8b8e
acupoftee <teedia31@gmail.com>
2019-07-01 09:40:15 -0400

    Add assets/scripts/game/gameEvents.js
    Adding gameEvents.js for game creation and retrieval.

---
+++ assets/scripts/game/gameEvents.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const api = require('./api')
+const ui = require('./ui')
+
+const onCreateGame = event => {
+  event.preventDefault()
+
+  api.createGame()
+    .then(ui.createGameSuccessful)
+    .catch(ui.createGameFailure)
+}
+
+const onGetStats = (event, id) => {
+  event.preventDefault()
+
+  api.getAllGames()
+    .then(ui.getAllGamesSuccess)
+    .catch(console.error)
+}
+
+module.exports = {
+  onCreateGame,
+  onGetStats
+}
--- curl-scripts/sign-out.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl --include --request DELETE "https://tic-tac-toe-wdi.herokuapp.com/sign-out" \
-  --header "Authorization: Token token=${TOKEN}"
-
-echo
