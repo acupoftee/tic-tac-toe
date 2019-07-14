8fd70b7af755dc681686fa6da9728e6793742e80
acupoftee <teedia31@gmail.com>
2019-07-14 16:19:10 -0400

    Add Theme Event Listeners in app.js
    Adding event listeners for themes in app.js.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,7 @@ const authEvents = require('./auth/authEvents')
 const gameEvents = require('./game/gameEvents')
 const boardEvents = require('./game/boardEvents')
 const styleEvents = require('./ui/styleEvents')
+const themeEvents = require('./ui/themeEvents')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
@@ -23,8 +24,19 @@ $(() => {
 
   $('.create-game').on('click', gameEvents.onCreateGame)
   $('#get-games').on('click', gameEvents.onGetStats)
-  $('#new-password').on('click', styleEvents.getPasswordForm)
+  $('#settings').on('click', styleEvents.getSettingsForm)
   $('#home').on('click', styleEvents.getHome)
+
+  // SETTINGS
+  $('#new-password').on('click', styleEvents.getPasswordForm)
+  $('#new-theme').on('click', styleEvents.getThemes)
+  $('#return').on('click', styleEvents.getHome)
+
+  // THEME EVENTS
+  $('.grape').on('click', themeEvents.changeToGrape)
+  $('.strawberry').on('click', themeEvents.changeToStrawberry)
+  $('.lime').on('click', themeEvents.changeToLime)
+  $('.blueberry').on('click', themeEvents.changeToBlueberry)
 
   // GAME EVENTS
   $('.back').on('click', styleEvents.getLandingPage)
